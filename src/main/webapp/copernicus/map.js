c3f4b18ad5395b662f782fc4b1d8ee82a386cd24
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-10-25 16:06:09 +0200

    Deployment.

--- src/main/webapp/copernicus/map.js
+++ src/main/webapp/copernicus/map.js
@@ -3,8 +3,8 @@ var map = document.getElementById("map");
 var countries = map.querySelectorAll("path");
 
 countries.forEach((country) => {
-    country.addEventListener("click", function(country){
-        console.log(country.id)
+    country.addEventListener("click", function(){
+        alert(this.id);
     });
 });
 
