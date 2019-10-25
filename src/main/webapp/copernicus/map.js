89185e5a26fd275715af6bbee713e1148f782b71
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-10-25 19:45:22 +0200

    Select test

--- src/main/webapp/copernicus/map.js
+++ src/main/webapp/copernicus/map.js
@@ -8,3 +8,33 @@ countries.forEach((country) => {
     });
 });
 
+// Previously selected country:
+var previous = {};
+
+// Get the country clicked:
+document.getElementById("svgMap").addEventListener("click", function(event) {
+
+    var country = event.target;
+
+    // Name and id of country
+    var id = event.target.id;
+    var name = event.target.name;
+
+    // Output name of country 
+    document.getElementById("mapOutput").innerHTML = "Country is: " + name;
+
+    // Default style of a country
+    var defaultStyle = "fill='#ececec';stroke-linejoin='round';stroke-width='.1';stroke='black';stroke-linecap='round';";
+
+    // Revert style of previous
+    previous.style = defaultStyle;
+
+    // Set new previous
+    previous = country;
+
+    // Set country style 
+    country.style = "fill:#0004ff;";
+
+    
+
+});
