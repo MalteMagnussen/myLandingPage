31ea7837453f0ca368f95387824167dfe33b8f21
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-10-26 10:52:29 +0200

    Added Wind Turbine and dynamic content loading.

--- src/main/webapp/copernicus/map.js
+++ src/main/webapp/copernicus/map.js
@@ -26,6 +26,7 @@ document.getElementById("svgMap").addEventListener("click", function (event) {
         country.style = "fill:#0004ff;";
         // Output name of country 
         document.getElementById("mapOutput").innerHTML = "Country is: " + id;
+        // ADD DYNAMIC BEHAVIOR HERE BASED ON ID. CALL NAVIGATE. 
     } else // if you've selected the ocean. 
     {
         document.getElementById("mapOutput").innerHTML = "Please select a country.";
@@ -34,17 +35,6 @@ document.getElementById("svgMap").addEventListener("click", function (event) {
 });
 
 /* The JavaScript Code for Navigation Dynamic Behavior */
-// Our Cache - Stores the partial .HTML pages.
-var partialsCache = {} // Important that this is at the top, or it won't work. 
-/* If no Fragment Identifier is provided then we default to home */
-if (!location.hash) { // Uses the falsy concept
-    location.hash = "#home";
-} 
-/* Navigate once to the initial hash value */
-navigate();
-/* Listen for fragment identifier value changes (The # at the end of the URL is the fragment) */
-/* Navigate whenever the fragment identifier value changes */
-window.addEventListener("hashchange", navigate);
 /* Encapsulates an HTTP GET request using XMLHttpRequest
 Fetches the file at the given path, then calls the 
 callback with the content of the file. */
@@ -54,46 +44,20 @@ function fetchFile(path, callback) {
     // Call the callback with the content loaded from the file. 
     request.onload = function () { // This is the function that gets invoked once the file is loaded.
         callback(request.responseText); // We get the content here. 
-    }; 
+    };
     // Fetch the partial HTML File given the fragment.
-    request.open("GET", path); // Initialize the request. HTTP GET request + PATH
+    request.open("GET", path + '.html'); // Initialize the request. HTTP GET request + PATH
     request.send(null); // Finalize the request.
-}
-
-/* Gets the appropriate content for the given fragment identifier */
-// Implements a cache.
-function getContent(fragment, callback) {
-    // If the page has been fetched before:
-    if (partialsCache[fragment]) {
-        // Just use the cached version:
-        callback(partialsCache[fragment]);
-    } else {
-        fetchFile(fragment + ".html", function (content) {
-            // Store the fetched content in the cache
-            partialsCache[fragment] = content;
-            // Pass the content to the callback
-            callback(content);
-        });
-    }
 }
 
 /* Updates Dynamic content based on the fragment identifier */
 // Is hoisted.
-function navigate() {
-    // Get a reference to the content Div
+function navigate(id) {
+    // Get a reference to the content Div - This is where we place the .html partial.
     var contentDiv = document.getElementById("content");
-    // Get a reference to the fragment. We use substr(1) to remove the '#' hash from the start of the string. 
-    var fragment = location.hash.substr(1);
     // Set the content div innerHTML based on the fragment identifier.
-    getContent(fragment, function (content) {
+    fetchFile(id, function (content) {
         contentDiv.innerHTML = content;
-        // INSERT HERE NEW CONTENT
-        switch (fragment) {
-            case "get":
-                endpoints();
-                break;
-        }
     });
-    
 }
 /* End of JavaScript code for dynamic navigation behavior */
