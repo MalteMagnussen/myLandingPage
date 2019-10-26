c0b1a4443e0ba189fdcc6e6f253fb90a46e7d4b5
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-10-26 18:03:19 +0200

    Added cards

--- src/main/webapp/copernicus/windspeeds.js
+++ src/main/webapp/copernicus/windspeeds.js
@@ -11,5 +11,6 @@ m2018150.addEventListener("click", function(){
     content.innerHTML = '<img src="../images/EUAvg2018150m.png" alt="Weather Data" style="max-width:100%;max-height:100%;height:auto;">'
 });
 
-// Output for content
-var content = document.getElementById("content");
+// Output for content for average wind speeds
+var content = document.getElementById("contentAvgWindSpeeds");
+
