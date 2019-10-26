91833cd233bdfb904a4209b7e3a717cf106ce3b2
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-10-26 18:14:06 +0200

    Update windspeeds.js

--- src/main/webapp/copernicus/windspeeds.js
+++ src/main/webapp/copernicus/windspeeds.js
@@ -2,24 +2,24 @@
 // 2018 100m button
 var m2018100 = document.getElementById("2018100m");
 m2018100.addEventListener("click", function(){
-    content.innerHTML = '<img src="../images/EUAvg2018100m.png" alt="Weather Data" style="max-width:100%;max-height:100%;height:auto;">'
+    contentWindspeed.innerHTML = '<img src="../images/EUAvg2018100m.png" alt="Weather Data" style="max-width:100%;max-height:100%;height:auto;">'
 });
 
 // 2018 150m button
 var m2018150 = document.getElementById("2018150m");
 m2018150.addEventListener("click", function(){
-    content.innerHTML = '<img src="../images/EUAvg2018150m.png" alt="Weather Data" style="max-width:100%;max-height:100%;height:auto;">'
+    contentWindspeed.innerHTML = '<img src="../images/EUAvg2018150m.png" alt="Weather Data" style="max-width:100%;max-height:100%;height:auto;">'
 });
 
 // Output for content for average wind speeds
-var content = document.getElementById("contentAvgWindSpeeds");
+var contentWindspeed = document.getElementById("contentAvgWindSpeeds");
 
 // Vestas 164, 100m button
 var m2018150 = document.getElementById("V164100m");
 m2018150.addEventListener("click", function(){
-    content.innerHTML = '<img src="../images/EUHourAvgV1642018100m.png" alt="Weather Data" style="max-width:100%;max-height:100%;height:auto;">'
+    contentProduction.innerHTML = '<img src="../images/EUHourAvgV1642018100m.png" alt="Weather Data" style="max-width:100%;max-height:100%;height:auto;">'
 });
 
 // Output for content for average wind speeds
-var content = document.getElementById("contentAvgProduction");
+var contentProduction = document.getElementById("contentAvgProduction");
 
