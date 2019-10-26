0640e6006e5541a2b982c496f7fc42a095ff23e9
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-10-26 15:00:58 +0200

    Upload images

---
+++ src/main/webapp/copernicus/windspeeds.js
@@ -0,0 +1,13 @@
+
+// 2018 100m button
+var m2018100 = document.getElementById("2018100m");
+
+
+// 2018 150m button
+var m2018150 = document.getElementById("2018150m");
+m2018150.addEventListener("click", function(){
+    content.innerHTML = '<img src="" alt="Weather Data">'
+});
+
+// Output for content
+var content = document.getElementById("content");
