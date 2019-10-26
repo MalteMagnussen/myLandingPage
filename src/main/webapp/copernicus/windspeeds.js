0c0d2cab0a354d783e35ca9fc9724a002a9eb2bb
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-10-26 15:22:33 +0200

    Fitted photos to div

--- src/main/webapp/copernicus/windspeeds.js
+++ src/main/webapp/copernicus/windspeeds.js
@@ -2,13 +2,13 @@
 // 2018 100m button
 var m2018100 = document.getElementById("2018100m");
 m2018100.addEventListener("click", function(){
-    content.innerHTML = '<img src="../images/EUAvg2018100m.png" alt="Weather Data">'
+    content.innerHTML = '<img src="../images/EUAvg2018100m.png" alt="Weather Data" style="max-width:100%;max-height:100%;height:auto;">'
 });
 
 // 2018 150m button
 var m2018150 = document.getElementById("2018150m");
 m2018150.addEventListener("click", function(){
-    content.innerHTML = '<img src="../images/EUAvg2018150m.png" alt="Weather Data">'
+    content.innerHTML = '<img src="../images/EUAvg2018150m.png" alt="Weather Data" style="max-width:100%;max-height:100%;height:auto;">'
 });
 
 // Output for content
