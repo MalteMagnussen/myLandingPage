7b2963073bf879dad76a4c217eb6861b3dc9dde4
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-10-26 18:09:48 +0200

    New Wind data

--- src/main/webapp/copernicus/windspeeds.js
+++ src/main/webapp/copernicus/windspeeds.js
@@ -14,3 +14,12 @@ m2018150.addEventListener("click", function(){
 // Output for content for average wind speeds
 var content = document.getElementById("contentAvgWindSpeeds");
 
+// Vestas 164, 100m button
+var m2018150 = document.getElementById("V164100m");
+m2018150.addEventListener("click", function(){
+    content.innerHTML = '<img src="../images/EUHourAvgV1642018100m.png" alt="Weather Data" style="max-width:100%;max-height:100%;height:auto;">'
+});
+
+// Output for content for average wind speeds
+var content = document.getElementById("contentAvgProduction");
+
